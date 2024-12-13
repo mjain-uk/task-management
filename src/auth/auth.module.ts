d73209139370339c5ccfdf691c73e9762f129d54
Madhur Jain <madhur.jain.uk@gmail.com>
2024-12-13 17:05:20 +0000

    feat: create boiler plate auth module with various layers

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { UsersRepository } from './users.respository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [AuthController],
+  providers: [AuthService, UsersRepository],
+})
+export class AuthModule {}
